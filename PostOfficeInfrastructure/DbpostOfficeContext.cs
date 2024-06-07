910a721b8ed1b31814938d2bd95e897f19bb382d
Ura3535 <urakovalchuk3535@gmail.com>
2024-06-07 17:49:49 +0300

    1

--- PostOfficeInfrastructure/DbpostOfficeContext.cs
+++ PostOfficeInfrastructure/DbpostOfficeContext.cs
@@ -31,7 +31,7 @@ public partial class DbpostOfficeContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-LGFLGFJ\\SQLEXPRESS; Database=DBPostOffice; Trusted_Connection=True; TrustServerCertificate=True; ");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-NAU7KE9\\SQLEXPRESS; Database=DBPostOffice; Trusted_Connection=True; TrustServerCertificate=True; ");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
